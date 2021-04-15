04aefd17841c1e575e09abb9fc3be2671c9b39db
tubasthebest <tubasthebest@gmail.com>
2021-04-14 20:37:17 -0600

    save and remove mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,37 @@ export const ADD_USER = gql`
         }
     }
 `;
+
+export const SAVE_BOOK = gql`
+	mutation saveBook($authors: [String]!, $bookId: Int!, $image: String!, $link: String!, $title: String!, $description: String!) {
+		saveBook(authors: $authors, bookId: $bookId, image: $image, link: $link, title: $title, description: $description) {
+			_id
+			username
+			savedBooks {
+				bookId
+				authors
+				description
+				image
+				link
+				title
+			}
+		}
+	}
+`;
+
+export const REMOVE_BOOK = gql`
+	mutation removeBook($bookId: ID!) {
+		removeBook(bookId: $bookId) {
+			_id
+			username
+			savedBooks {
+				authors
+				description
+				bookId
+				image
+				link
+				title
+			}
+		}
+	}
+`;
